298be9b9040fbb03292580a265616246321f2736
daniel <daniel@oikarainen.se>
2023-06-11 20:51:03 +0200

    bootstreap nav implemented

--- wp-content/themes/danielthemeadapt/functions.php
+++ wp-content/themes/danielthemeadapt/functions.php
@@ -1,4 +1,4 @@
-<?php
+ <?php
 
 //Useful of printing/debugging apps
 //echo '<pre>';
--- wp-content/themes/danielthemeadapt/header.php
+++ wp-content/themes/danielthemeadapt/header.php
@@ -20,15 +20,16 @@
 <!-- Add wordpress classes- Adds classes to every page(even possible to add custom ones like lasanga:) -->
 <body <?php body_class( 'lasagnafood'); ?>>
     <!-- Inject code just AFTER body tag - Enables to hook into action wp_body_open -->
-    <?php wp_body_open(); ?>
+    <?php 
+        //For backward compatibility, ensure php > 5.2
+        if (function_exists('wp_body_open')){
+            wp_body_open(); 
+        }
+    ?>
     <!-- Add your header content here -->
     <header>
         HEADER
-        <nav>
-            <ul>
-                <li><a href="link1.html">Link 1</a></li>
-                <li><a href="link2.html">Link 2</a></li>
-                <li><a href="link3.html">Link 3</a></li>
-            </ul>
-        </nav>
+
+        <?php get_template_part( 'template-parts/header/nav' ); ?>
+   
     </header>
